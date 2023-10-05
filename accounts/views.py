25841e764c11ddf03b5ba932664f4d0ef5c0be63
Ashish Karhade <akarhade5@gmail.com>
2023-10-05 22:39:16 +0530

    register_user view created

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-
+from .forms import UserForm
+from django.shortcuts import redirect
+from .models import User
 # Create your views here.
 
+
 def register_user(request):
-    return render(request, 'accounts/register_user.html')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            confirm_password = form.cleaned_data['confirm_password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.role = User.CUSTOMER
+            user.save()
+            return redirect('registerUser')
+    else:
+        form = UserForm
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register_user.html', context=context)
 
 
 def register_restaurant(request):
